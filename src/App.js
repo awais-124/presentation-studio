784a1406ca3bff6250aac512675c91bfcb699ec5
awais-124 <awais14940@gmail.com>
2024-12-29 18:42:42 +0500

    Removed error

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import './App.css';
 import Nav from './sections/Nav/nav';
 import Hero from './sections/Hero/hero';
 import Features from './sections/Features/features';
-import PopularSlides from './sections/Popular-Slides/popular-slides';
 import Categories from './sections/Categories/categories';
 import Pricing from './sections/Pricing/pricing';
-import Working from './sections/Working/working';
 import Testimonial from './sections/Testimonial/testimonial';
-import FAQs from './sections/FAQs/FAQs';
-import Contact from './sections/Contact/contact';
 import Footer from './sections/Footer/footer';
 
 function App() {
@@ -19,13 +15,9 @@ function App() {
       <Nav />
       <Hero />
       <Features />
-      {/* <PopularSlides /> */}
       <Categories />
       <Pricing />
-      {/* <Working /> */}
       <Testimonial />
-      <FAQs />
-      <Contact />
       <Footer />
     </React.Fragment>
   );

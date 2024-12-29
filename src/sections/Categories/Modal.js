b779d60543ad94b597fdcff8eadaa86b29017f88
awais-124 <awais14940@gmail.com>
2024-12-29 22:53:52 +0500

    Removed warnings from Modal.js

--- src/sections/Categories/Modal.js
+++ src/sections/Categories/Modal.js
@@ -2,31 +2,6 @@ import React from 'react';
 import classes from './Modal.module.css';
 import { slidesLinks } from '../../helpers/constants';
 
-const pptLinks = [
-  'https://docs.google.com/presentation/d/1QhjkDJ94TBjQhB1vS5Gyr5uE9RUSWWKE/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/1mvHmRB8SKR41PsbcRrAQoxr5HT9oIQMl/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/1KFf6PawzoQj-gCOALKZzb2RiQX_kwUoz/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/1oqP1WLKYZWreEkihOPr5bbDlbwrpGt2_/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/1QoLpPxzr0hGtxwHuYeRccwA_dmTejGoM/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/1iUNP1zvgjfXfpDU2aWbeWNEufijM_MO9/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/1gn5oKXlEuddjX5qiQibvR0WsMv1mN-F9/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/18mIszmpDyJ8VvaXPXY7nIAY7DbOMsLQw/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/1Rd-Ls1VGFGtWRlaHTVlNNrc7MVpLZUkA/edit?usp=drive_link',
-  'https://docs.google.com/presentation/d/1HSyZF1QUAFUiM5EQDK7Te0ZoIJiFpefw/edit?usp=drive_link',
-];
-
-const downloadLinks = [
-  'https://drive.google.com/uc?export=download&id=1QhjkDJ94TBjQhB1vS5Gyr5uE9RUSWWKE',
-  'https://drive.google.com/uc?export=download&id=1mvHmRB8SKR41PsbcRrAQoxr5HT9oIQMl',
-  'https://drive.google.com/uc?export=download&id=1KFf6PawzoQj-gCOALKZzb2RiQX_kwUoz',
-  'https://drive.google.com/uc?export=download&id=1oqP1WLKYZWreEkihOPr5bbDlbwrpGt2_',
-  'https://drive.google.com/uc?export=download&id=1QoLpPxzr0hGtxwHuYeRccwA_dmTejGoM',
-  'https://drive.google.com/uc?export=download&id=1iUNP1zvgjfXfpDU2aWbeWNEufijM_MO9',
-  'https://drive.google.com/uc?export=download&id=1gn5oKXlEuddjX5qiQibvR0WsMv1mN-F9',
-  'https://drive.google.com/uc?export=download&id=18mIszmpDyJ8VvaXPXY7nIAY7DbOMsLQw',
-  'https://drive.google.com/uc?export=download&id=1Rd-Ls1VGFGtWRlaHTVlNNrc7MVpLZUkA',
-  'https://drive.google.com/uc?export=download&id=1HSyZF1QUAFUiM5EQDK7Te0ZoIJiFpefw',
-];
 
 function Modal({ category, closeModal }) {
   function getRandomColor() {

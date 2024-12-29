f1afb5ec9ee5919597d04e8b21f1c21eb50492f7
awais-124 <awais14940@gmail.com>
2024-12-29 21:53:03 +0500

    Added nav-logo

--- src/sections/Nav/nav.js
+++ src/sections/Nav/nav.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import classes from './nav.module.css';
 
 import { navLinks } from '../../helpers/constants';
+import LOGO from '../../assets/logos/2.png';
 
 function Nav({ signout }) {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -10,29 +11,33 @@ function Nav({ signout }) {
   return (
     <nav className={classes.navbar}>
       <div className={classes.logo}>
-        Presentation<span>Studio</span>
+        <img src={LOGO} alt='logo' />
       </div>
-      <ul className={`${classes.navLinks} ${isMenuOpen ? classes.active : ''}`}>
-        {navLinks.map((link) => (
-          <li key={link.id}>
-            <a
-              href={`#${link.id}`}
-              className={classes.link}
-              onClick={() => setIsMenuOpen(false)}
-            >
-              {link.label}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <div className={classes.width}>
+        <ul
+          className={`${classes.navLinks} ${isMenuOpen ? classes.active : ''}`}
+        >
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a
+                href={`#${link.id}`}
+                className={classes.link}
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {link.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className={classes.hamburger} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </div>
       <button onClick={signout} className={classes.logout}>
         LOGOUT
       </button>
-      <div className={classes.hamburger} onClick={toggleMenu}>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
     </nav>
   );
 }
